6a8e8a9264259431ce828dd8fa367d5a52fafc72
Valter Uotila <valter.uotila@gmail.com>
2018-05-31 18:23:15 +0300

    Final?

--- controllers/login.js
+++ controllers/login.js
@@ -5,22 +5,27 @@ const User = require('../models/user')
 const Location = require('../models/location')
 
 loginRouter.post('/', async (request, response) => {
-	const body = request.body
-	const user = await User.findOne({ username: body.username })
-	const passwordCorrect = user === null ? false : await bcrypt.compare(body.password, user.password)
+	try {
+		const body = request.body
+		const user = await User.findOne({ username: body.username })
+		const passwordCorrect = user === null ? false : await bcrypt.compare(body.password, user.password)
 
-	if ( !(user && passwordCorrect) ) {
-		return response.status(401).send({ error: 'Invalid username or password' })
+		if ( !(user && passwordCorrect) ) {
+			return response.status(401).send({ error: 'Invalid username or password' })
+		}
+
+		const userForToken = {
+			username: user.username,
+			id: user._id
+		}
+
+		const token = jwt.sign(userForToken, process.env.SECRET)
+		const userLocations = await Location.findById(user.locations)
+		response.status(200).send({ token, username: user.username, name: user.name, locations: userLocations })
+	} catch (error) {
+		console.log(error)
+		return response.status(404).end()
 	}
-
-	const userForToken = {
-		username: user.username,
-		id: user._id
-	}
-
-	const token = jwt.sign(userForToken, process.env.SECRET)
-	const userLocations = await Location.findById(user.locations)
-	response.status(200).send({ token, username: user.username, name: user.name, locations: userLocations })
 })
 
 module.exports = loginRouter
